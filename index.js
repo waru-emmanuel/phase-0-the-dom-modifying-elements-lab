0880f95e32e36001b42af5d0786dba76409c8bb9
wachira <wewaruts@gmail.com>
2024-04-07 20:25:46 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+/*const body = document.body
+body.append ("Hi, welcome to Moringa")
+*/
+
+const mainElement = document.getElementById('main');
+
+// Check if the main element exists
+if (mainElement) {
+    // Remove the main element from the DOM
+    mainElement.remove();
+}
+   const newHeader = document.createElement('h1'); //'newHeader' variable that points to an <h1> node"
+   newHeader.textContent = 'WACHIRA is the champion';
+   newHeader.id = "victory"
+   //document.body.appendChild(newHeader);
